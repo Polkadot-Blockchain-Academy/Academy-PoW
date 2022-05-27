61ed22f916c17959f0740c56fee580344da6e959
Joshy Orndorff <admin@joshyorndorff.com>
2022-05-27 16:20:39 -0400

    First pass at service

--- node/src/service.rs
+++ node/src/service.rs
@@ -4,25 +4,30 @@ use std::sync::Arc;
 use sc_client_api::ExecutorProvider;
 use utxo_runtime::{self, opaque::Block, RuntimeApi};
 use sc_service::{error::Error as ServiceError, Configuration, PartialComponents, TaskManager};
-use sp_inherents::InherentDataProviders;
-use sc_executor::native_executor_instance;
-pub use sc_executor::NativeExecutor;
+use sc_executor::NativeElseWasmExecutor;
 use sha3pow::Sha3Algorithm;
-use sc_network::{config::DummyFinalityProofRequestBuilder};
 use core::clone::Clone;
 use sp_core::sr25519;
 use parity_scale_codec::Encode;
-use sp_consensus::import_queue::BasicQueue;
-use sp_api::TransactionFor;
-use sc_client_api::backend::RemoteBackend;
+use sc_telemetry::{Telemetry, TelemetryWorker};
 
 // Our native executor instance.
-native_executor_instance!(
-	pub Executor,
-	utxo_runtime::api::dispatch,
-	utxo_runtime::native_version,
-);
+pub struct ExecutorDispatch;
 
+impl sc_executor::NativeExecutionDispatch for ExecutorDispatch {
+
+	fn dispatch(method: &str, data: &[u8]) -> Option<Vec<u8>> {
+		utxo_runtime::api::dispatch(method, data)
+	}
+
+	fn native_version() -> sc_executor::NativeVersion {
+		utxo_runtime::native_version()
+	}
+}
+
+//TODO We'll need the mining worker. Can probably copy from recipes
+
+// TODO This will need to be reworked significantly
 pub fn build_inherent_data_providers(sr25519_public_key: sr25519::Public) -> Result<InherentDataProviders, ServiceError> {
 	let providers = InherentDataProviders::new();
 
@@ -41,7 +46,7 @@ pub fn build_inherent_data_providers(sr25519_public_key: sr25519::Public) -> Res
 	Ok(providers)
 }
 
-type FullClient = sc_service::TFullClient<Block, RuntimeApi, Executor>;
+type FullClient = sc_service::TFullClient<Block, RuntimeApi, NativeElseWasmExecutor<ExecutorDispatch>>;
 type FullBackend = sc_service::TFullBackend<Block>;
 type FullSelectChain = sc_consensus::LongestChain<FullBackend, Block>;
 
@@ -49,22 +54,58 @@ type FullSelectChain = sc_consensus::LongestChain<FullBackend, Block>;
 /// But enough to perform chain operations like purge-chain
 pub fn new_partial(config: &Configuration, sr25519_public_key: sr25519::Public) -> Result<
 	PartialComponents<
-		FullClient, FullBackend, FullSelectChain,
-		BasicQueue<Block, TransactionFor<FullClient, Block>>,
+		FullClient,
+		FullBackend,
+		FullSelectChain,
+		sc_consensus::DefaultImportQueue<Block, FullClient>,
 		sc_transaction_pool::FullPool<Block, FullClient>,
-		sc_consensus_pow::PowBlockImport<Block, Arc<FullClient>, FullClient, FullSelectChain, Sha3Algorithm<FullClient>, impl sp_consensus::CanAuthorWith<Block>>,
+		(
+			sc_consensus_pow::PowBlockImport<
+				Block,
+				Arc<FullClient>,
+				FullClient,
+				FullSelectChain,
+				Sha3Algorithm<FullClient>,
+				impl sp_consensus::CanAuthorWith<Block>,
+			>,
+			Option<Telemetry>,
+		),
 	>,
-ServiceError> {
-	let inherent_data_providers = build_inherent_data_providers(sr25519_public_key)?;
+	ServiceError,
+> {
+	//let inherent_data_providers = build_inherent_data_providers(sr25519_public_key)?;
 
-	let (client, backend, keystore, task_manager) =
-		sc_service::new_full_parts::<Block, RuntimeApi, Executor>(&config)?;
-	let client : std::sync::Arc<_> = Arc::new(client);
+	let telemetry = config
+		.telemetry_endpoints
+		.clone()
+		.filter(|x| !x.is_empty())
+		.map(|endpoints| -> Result<_, sc_telemetry::Error> {
+			let worker = TelemetryWorker::new(16)?;
+			let telemetry = worker.handle().new_telemetry(endpoints);
+			Ok((worker, telemetry))
+		})
+		.transpose()?;
+
+	let executor = NativeElseWasmExecutor::<ExecutorDispatch>::new(
+		config.wasm_method,
+		config.default_heap_pages,
+		config.max_runtime_instances,
+		config.runtime_cache_size,
+	);
+
+	let (client, backend, keystore_container, task_manager) =
+		sc_service::new_full_parts::<Block, RuntimeApi, _>(
+			&config,
+			telemetry.as_ref().map(|(_, telemetry)| telemetry.handle()),
+			executor,
+		)?;
+	let client = Arc::new(client);
 
 	let select_chain = sc_consensus::LongestChain::new(backend.clone());
 
 	let transaction_pool = sc_transaction_pool::BasicPool::new_full(
 		config.transaction_pool.clone(),
+		config.role.is_authority().into(),
 		config.prometheus_registry(),
 		task_manager.spawn_handle(),
 		client.clone(),
@@ -86,65 +127,72 @@ ServiceError> {
 	let import_queue = sc_consensus_pow::import_queue(
 		Box::new(pow_block_import.clone()),
 		None,
-		None,
 		Sha3Algorithm::new(client.clone()),
-		inherent_data_providers.clone(),
 		&task_manager.spawn_handle(),
 		config.prometheus_registry(),
 	)?;
 
 	Ok(PartialComponents {
-		client, backend, import_queue, keystore, task_manager, transaction_pool,
-		select_chain, inherent_data_providers,
-		other: pow_block_import,
+		client,
+		backend,
+		task_manager,
+		import_queue,
+		keystore_container,
+		select_chain,
+		transaction_pool,
+		other: (pow_block_import, telemetry),
 	})
 }
 
 /// Builds a new service for a full client.
 pub fn new_full(config: Configuration, sr25519_public_key: sr25519::Public) -> Result<TaskManager, ServiceError> {
 	let sc_service::PartialComponents {
-		client, backend, mut task_manager, import_queue, keystore, select_chain, transaction_pool,
-		inherent_data_providers,
-		other: pow_block_import,
+		client,
+		backend,
+		mut task_manager,
+		import_queue,
+		mut keystore_container,
+		select_chain,
+		transaction_pool,
+		other: (pow_block_import, mut telemetry),
 	} = new_partial(&config, sr25519_public_key)?;
 
-	let (network, network_status_sinks, system_rpc_tx, network_starter) =
+	let (network, system_rpc_tx, network_starter) =
 		sc_service::build_network(sc_service::BuildNetworkParams {
 			config: &config,
 			client: client.clone(),
 			transaction_pool: transaction_pool.clone(),
 			spawn_handle: task_manager.spawn_handle(),
 			import_queue,
-			on_demand: None,
 			block_announce_validator_builder: None,
-			finality_proof_request_builder: None,
-			finality_proof_provider: None,
+			warp_sync: None,
 		})?;
 
 	let role = config.role.clone();
 	let prometheus_registry = config.prometheus_registry().cloned();
-	let telemetry_connection_sinks = sc_service::TelemetryConnectionSinks::default();
 
 	sc_service::spawn_tasks(sc_service::SpawnTasksParams {
 		network: network.clone(),
 		client: client.clone(),
-		keystore: keystore.clone(),
+		keystore: keystore_container.sync_keystore(),
 		task_manager: &mut task_manager,
 		transaction_pool: transaction_pool.clone(),
-		telemetry_connection_sinks: telemetry_connection_sinks.clone(),
 		rpc_extensions_builder: Box::new(|_, _| ()),
-		on_demand: None,
-		remote_blockchain: None,
-		backend, network_status_sinks, system_rpc_tx, config,
+		backend,
+		system_rpc_tx,
+		config,
+		telemetry: telemetry.as_mut(),
 	})?;
 
 
 
 	if role.is_authority() {
 		let proposer = sc_basic_authorship::ProposerFactory::new(
+			task_manager.spawn_handle(),
 			client.clone(),
 			transaction_pool,
 			prometheus_registry.as_ref(),
+			telemetry.as_ref().map(|x| x.handle()),
 		);
 
 		// The number of rounds of mining to try in a single call
@@ -153,6 +201,7 @@ pub fn new_full(config: Configuration, sr25519_public_key: sr25519::Public) -> R
 		let can_author_with =
 			sp_consensus::CanAuthorWithNativeVersion::new(client.executor().clone());
 
+		//TODO I think this needs to be re-worked.
 		sc_consensus_pow::start_mine(
 			Box::new(pow_block_import),
 			client.clone(),
@@ -172,79 +221,4 @@ pub fn new_full(config: Configuration, sr25519_public_key: sr25519::Public) -> R
 
 	network_starter.start_network();
 	Ok(task_manager)
-}
-
-/// Builds a new service for a light client.
-pub fn new_light(config: Configuration, sr25519_public_key: sr25519::Public) -> Result<TaskManager, ServiceError> {
-	let (client, backend, keystore, mut task_manager, on_demand) =
-		sc_service::new_light_parts::<Block, RuntimeApi, Executor>(&config)?;
-
-	let transaction_pool = Arc::new(sc_transaction_pool::BasicPool::new_light(
-		config.transaction_pool.clone(),
-		config.prometheus_registry(),
-		task_manager.spawn_handle(),
-		client.clone(),
-		on_demand.clone(),
-	));
-
-	let select_chain = sc_consensus::LongestChain::new(backend.clone());
-	let inherent_data_providers = build_inherent_data_providers(sr25519_public_key)?;
-	let can_author_with =
-		sp_consensus::CanAuthorWithNativeVersion::new(client.executor().clone());
-
-	let pow_block_import = sc_consensus_pow::PowBlockImport::new(
-		client.clone(),
-		client.clone(),
-		Sha3Algorithm::new(client.clone()),
-		0, // check inherents starting at block 0
-		Some(select_chain),
-		inherent_data_providers.clone(),
-		// TODO what should I Really use here? This compiles, and the `can_author_with` from line 196 doesn't??
-		sp_consensus::AlwaysCanAuthor,
-	);
-
-	let import_queue = sc_consensus_pow::import_queue(
-		Box::new(pow_block_import),
-		None,
-		None,
-		Sha3Algorithm::new(client.clone()),
-		inherent_data_providers.clone(),
-		&task_manager.spawn_handle(),
-		config.prometheus_registry(),
-	)?;
-
-	let fprb = Box::new(DummyFinalityProofRequestBuilder::default()) as Box<_>;
-
-	let (network, network_status_sinks, system_rpc_tx, network_starter) =
-		sc_service::build_network(sc_service::BuildNetworkParams {
-			config: &config,
-			client: client.clone(),
-			transaction_pool: transaction_pool.clone(),
-			spawn_handle: task_manager.spawn_handle(),
-			import_queue,
-			on_demand: Some(on_demand.clone()),
-			block_announce_validator_builder: None,
-			finality_proof_request_builder: Some(fprb),
-			finality_proof_provider: None,
-		})?;
-
-	sc_service::spawn_tasks(sc_service::SpawnTasksParams {
-		remote_blockchain: Some(backend.remote_blockchain()),
-		transaction_pool,
-		task_manager: &mut task_manager,
-		on_demand: Some(on_demand),
-		rpc_extensions_builder: Box::new(|_, _| ()),
-		telemetry_connection_sinks: sc_service::TelemetryConnectionSinks::default(),
-		config,
-		client,
-		keystore,
-		backend,
-		network,
-		network_status_sinks,
-		system_rpc_tx,
-	 })?;
-
-	 network_starter.start_network();
-
-	 Ok(task_manager)
 }
