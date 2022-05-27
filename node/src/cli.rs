53368e69575e718ddc586da804d957155515c75b
Joshy Orndorff <admin@joshyorndorff.com>
2022-05-27 16:12:51 -0400

    structopt -> clap

--- node/src/cli.rs
+++ node/src/cli.rs
@@ -1,23 +1,21 @@
-pub use sc_cli::Subcommand;
 use std::convert::TryInto;
-use structopt::StructOpt;
 
-#[derive(Debug, StructOpt)]
+#[derive(Debug, clap::Parser)]
 pub struct Cli {
-	#[structopt(subcommand)]
+	#[clap(subcommand)]
 	pub subcommand: Option<Subcommand>,
 
-	#[structopt(flatten)]
+	#[clap(flatten)]
 	pub run: RunCmd,
 }
 
-#[derive(Debug, StructOpt)]
+#[derive(Debug, clap::Parser)]
 pub struct RunCmd {
-	#[structopt(flatten)]
+	#[clap(flatten)]
 	pub base: sc_cli::RunCmd,
 
 	/// Miner's SR25519 public key for block rewards
-	#[structopt(long, parse(try_from_str = parse_sr25519_public_key))]
+	#[clap(long, parse(try_from_str = parse_sr25519_public_key))]
 	pub sr25519_public_key: Option<sp_core::sr25519::Public>,
 }
 
@@ -27,4 +25,28 @@ fn parse_sr25519_public_key(i: &str) -> Result<sp_core::sr25519::Public, String>
 		.as_slice()
 		.try_into()
 		.or(Err("invalid length for SR25519 public key".to_string()))
+}
+
+#[derive(Debug, clap::Subcommand)]
+pub enum Subcommand {
+	/// Build a chain specification.
+	BuildSpec(sc_cli::BuildSpecCmd),
+
+	/// Validate blocks.
+	CheckBlock(sc_cli::CheckBlockCmd),
+
+	/// Export blocks.
+	ExportBlocks(sc_cli::ExportBlocksCmd),
+
+	/// Export the state of a given block into a chain spec.
+	ExportState(sc_cli::ExportStateCmd),
+
+	/// Import blocks.
+	ImportBlocks(sc_cli::ImportBlocksCmd),
+
+	/// Remove the whole chain.
+	PurgeChain(sc_cli::PurgeChainCmd),
+
+	/// Revert the chain to a previous state.
+	Revert(sc_cli::RevertCmd),
 }
