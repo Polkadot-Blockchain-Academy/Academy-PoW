eac5b7fffc2f6acafb9d78d7574bbd0e254dedbe
filip <fbielejec@gmail.com>
2023-06-14 14:56:30 +0200

    docker

--- Makefile
+++ Makefile
@@ -12,3 +12,9 @@ watch:
 
 release:
 	WASM_BUILD_WORKSPACE_HINT=${PWD} CARGO_TARGET_DIR=/tmp/academy-pow/target/ cargo build --release
+
+image:
+	mkdir --parents /tmp/academy-pow/docker/ && \
+	cp docker/docker_entrypoint.sh /tmp/academy-pow/docker/docker_entrypoint.sh && \
+	docker build --tag academy-pow-node:latest -f ${PWD}/docker/Dockerfile /tmp  && \
+	docker image tag academy-pow-node:latest academy-pow-node:$(shell git rev-parse --short=10 HEAD)
---
+++ docker/docker_entrypoint.sh
@@ -0,0 +1,78 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# load env variables from a file if ENV_FILE is set
+if [[ -n "${ENV_FILE:-}" ]] && [[ -f "${ENV_FILE}" ]]; then
+  set -o allexport
+  source ${ENV_FILE}
+  set +o allexport
+fi
+
+# script env variables with defaults
+PURGE_BEFORE_START=${PURGE_BEFORE_START:-}
+ALLOW_PRIVATE_IPV4=${ALLOW_PRIVATE_IPV4:-}
+DISCOVER_LOCAL=${DISCOVER_LOCAL:-}
+
+# cli options to env variables
+CHAIN=${CHAIN:?'Chain should be specified'}
+NAME=${NAME:?'Name should be specified'}
+BASE_PATH=${BASE_PATH:?'Base path should be specified'}
+RPC_PORT=${RPC_PORT:-9933}
+WS_PORT=${WS_PORT:-9943}
+PORT=${PORT:-30333}
+VALIDATOR=${VALIDATOR:-true}
+WS_MAX_CONNECTIONS=${WS_MAX_CONNECTIONS:-100}
+POOL_LIMIT=${POOL_LIMIT:-1024}
+DB_CACHE=${DB_CACHE:-1024}
+RUNTIME_CACHE_SIZE=${RUNTIME_CACHE_SIZE:-2}
+MAX_RUNTIME_INSTANCES=${MAX_RUNTIME_INSTANCES:-8}
+
+ARGS=(
+  --execution Native
+  --name "${NAME}"
+  --base-path "${BASE_PATH}"
+  --pool-limit "${POOL_LIMIT}"
+  --chain "${CHAIN}"
+  --node-key-file "${NODE_KEY_PATH}"
+  --rpc-port "${RPC_PORT}"
+  --ws-port "${WS_PORT}"
+  --port "${PORT}"
+  --rpc-cors all
+  --no-mdns
+  --ws-max-connections "${WS_MAX_CONNECTIONS}"
+  --unsafe-ws-external --unsafe-rpc-external
+  --enable-log-reloading
+  --db-cache "${DB_CACHE}"
+  --runtime-cache-size "${RUNTIME_CACHE_SIZE}"
+  --max-runtime-instances "${MAX_RUNTIME_INSTANCES}"
+  --detailed-log-output
+  --no-prometheus
+  --no-telemetry
+)
+
+if [[ -n "${BOOT_NODES:-}" ]]; then
+  ARGS+=(--bootnodes ${BOOT_NODES})
+fi
+
+if [[ -n "${RESERVED_NODES:-}" ]]; then
+  ARGS+=(--reserved-nodes "${RESERVED_NODES}")
+fi
+
+if [[ -n "${RESERVED_ONLY:-}" ]]; then
+  ARGS+=(--reserved-only)
+fi
+
+if [[ -n "${PUBLIC_ADDR:-}" ]]; then
+  ARGS+=(--public-addr "${PUBLIC_ADDR}")
+fi
+
+if [[ "true" == "$ALLOW_PRIVATE_IPV4" ]]; then
+  ARGS+=(--allow-private-ipv4)
+fi
+
+if [[ "true" == "$DISCOVER_LOCAL" ]]; then
+  ARGS+=(--discover-local)
+fi
+
+echo "${CUSTOM_ARGS}" | xargs aleph-node "${ARGS[@]}"
