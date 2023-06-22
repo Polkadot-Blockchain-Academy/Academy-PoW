3cfce2f272f03194966dc99aa74b050ee47707df
Piotr Mikołajczyk <piomiko41@gmail.com>
2023-06-22 17:53:52 +0200

    without alias?

--- .github/scripts/interact-with-contract.sh
+++ .github/scripts/interact-with-contract.sh
@@ -5,16 +5,16 @@ set -eo pipefail
 mkdir --parents test-contract/
 cd test-contract/
 
-alias cc='docker run --network host -u 1000:1000 --rm -it -v $(pwd):/sources paritytech/contracts-ci-linux:9a513893-20230620'
+CC="docker run --network host -u 1000:1000 --rm -it -v $(pwd):/sources paritytech/contracts-ci-linux:9a513893-20230620"
 
 # Create a new contract
-cc cargo contract new --target-dir /sources flipper
+$CC cargo contract new --target-dir /sources flipper
 
 # Build the contract
-cc cargo contract build --release --manifest-path=/sources/flipper/Cargo.toml
+$CC cargo contract build --release --manifest-path=/sources/flipper/Cargo.toml
 
 # Upload and instantiate code
-ADDRESS=$(cc cargo contract instantiate --suri //Alice --skip-confirm --args false -x --output-json /sources/flipper/target/ink/flipper.wasm | jq -jr '.contract')
+ADDRESS=$($CC cargo contract instantiate --suri //Alice --skip-confirm --args false -x --output-json /sources/flipper/target/ink/flipper.wasm | jq -jr '.contract')
 
 # Call the contract
-cc cargo contract call --suri //Alice --contract $ADDRESS --message flip --manifest-path=/sources/flipper/Cargo.toml -x --skip-confirm
+$CC cargo contract call --suri //Alice --contract $ADDRESS --message flip --manifest-path=/sources/flipper/Cargo.toml -x --skip-confirm
