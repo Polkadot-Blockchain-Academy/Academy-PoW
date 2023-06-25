b312c05e89e6c34225471078e312de47d72fb315
Joshy Orndorff <admin@joshyorndorff.com>
2023-06-25 12:11:55 -0400

    spec name as frontier template to trick apps into using ethereum style accounts

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -102,8 +102,8 @@ pub mod opaque {
 
 /// This runtime version.
 pub const VERSION: RuntimeVersion = RuntimeVersion {
-    spec_name: create_runtime_str!("academy-pow"),
-    impl_name: create_runtime_str!("academy-pow"),
+    spec_name: create_runtime_str!("frontier-template"),
+    impl_name: create_runtime_str!("frontier-template"),
     authoring_version: 1,
     spec_version: 1,
     impl_version: 1,
