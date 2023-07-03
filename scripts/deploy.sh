4041ccdfbb0bf9872a43b90db72e9baaf407915e
filip <fbielejec@gmail.com>
2023-07-03 11:07:59 +0200

    deploy dex

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -59,6 +59,12 @@ cargo_contract build --release
 PSP22_CODE_HASH=$(cargo_contract upload --url "$NODE" --suri "$AUTHORITY_SEED" --output-json --execute | jq -s . | jq -r '.[1].code_hash')
 
 TOKEN_ONE=$(cargo_contract instantiate --url "$NODE" --constructor new --args $TOTAL_SUPPLY --suri "$AUTHORITY_SEED" --salt 0x0001 --skip-confirm --output-json --execute | jq -r '.contract')
+TOKEN_TWO=$(cargo_contract instantiate --url "$NODE" --constructor new --args $TOTAL_SUPPLY --suri "$AUTHORITY_SEED" --salt 0x0002 --skip-confirm --output-json --execute | jq -r '.contract')
+
+cd "$CONTRACTS_PATH"/simple-dex
+cargo_contract build --release
+DEX_CODE_HASH=$(cargo_contract upload --url "$NODE" --suri "$AUTHORITY_SEED" --output-json --execute | jq -s . | jq -r '.[1].code_hash')
+DEX=$(cargo_contract instantiate --url "$NODE" --constructor new --args 0 "[$TOKEN_ONE,$TOKEN_TWO]" --suri "$AUTHORITY_SEED" --salt 0x0001 --skip-confirm --output-json --execute | jq -r '.contract')
 
 # spit adresses to a JSON file
 cd "$CONTRACTS_PATH"
@@ -67,12 +73,18 @@ jq -n \
    --arg roulette "$ROULETTE" \
    --arg roulette_code_hash "$ROULETTE_CODE_HASH" \
    --arg token_one "$TOKEN_ONE" \
+   --arg token_two "$TOKEN_TWO" \
    --arg psp22_code_hash "$PSP22_CODE_HASH" \
+   --arg dex "$DEX" \
+   --arg dex_code_hash "$DEX_CODE_HASH" \
    '{
       roulette: $roulette,
       roulette_code_hash: $roulette_code_hash,
       token_one: $token_one,
-      psp22_code_hash: $psp22_code_hash
+      token_two: $token_two,
+      psp22_code_hash: $psp22_code_hash,
+      dex: $dex,
+      dex_code_hash: $dex_code_hash
     }' > addresses.json
 
 cat addresses.json
