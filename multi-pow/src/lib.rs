7d24648a9bc406ec9fe07fae48a40c0f19637bba
Joshy Orndorff <admin@joshyorndorff.com>
2023-07-21 10:35:58 -0700

    allow all three seals

--- multi-pow/src/lib.rs
+++ multi-pow/src/lib.rs
@@ -242,11 +242,11 @@ where
 
         // To begin with we only allow md5 hashes for our pow
         // After the fork height this check is skipped so all the hashes become valid
-        match seal.work.algo {
-            SupportedHashes::Md5 => (),
-            SupportedHashes::Sha3 => return Ok(false),
-            SupportedHashes::Keccak => return Ok(false),
-        }
+        // match seal.work.algo {
+        //     SupportedHashes::Md5 => (),
+        //     SupportedHashes::Sha3 => (return Ok(false)),
+        //     SupportedHashes::Keccak => return Ok(false),
+        // }
 
         // See whether the hash meets the difficulty requirement. If not, fail fast.
         if !multi_hash_meets_difficulty(&seal.work, difficulty) {
