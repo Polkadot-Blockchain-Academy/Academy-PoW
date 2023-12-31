4c615b3e9000fac8a029638e0d2692d8e3760a34
Kevin Coxe <kevcoxe@gmail.com>
2023-12-31 02:15:57 -0500

    add try catch and fix svg

--- fork-visualizer/src/app/components/NodeTracker.jsx
+++ fork-visualizer/src/app/components/NodeTracker.jsx
@@ -19,21 +19,25 @@ export function NodeState ({ wsAddress, updateStuff }) {
         let count = 0
         console.log("subscribing...", wsAddress)
 
-        const wsProvider = new WsProvider(wsAddress)
-        const api = await ApiPromise.create({ provider: wsProvider })
-        const unsubscribe = await api.rpc.chain.subscribeNewHeads(async (header) => {
-            await updateStuff(header, api, wsAddress)
+        try {
+            const wsProvider = new WsProvider(wsAddress)
+            const api = await ApiPromise.create({ provider: wsProvider })
+            const unsubscribe = await api.rpc.chain.subscribeNewHeads(async (header) => {
+                await updateStuff(header, api, wsAddress)
 
-            if (++count === MAX_CHAIN_COUNT) {
-                setLoading(true)
-                unsubscribe();
-                setIsSubscribed(false)
-                setLoading(false)
-            }
-        });
+                if (++count === MAX_CHAIN_COUNT) {
+                    setLoading(true)
+                    unsubscribe();
+                    setIsSubscribed(false)
+                    setLoading(false)
+                }
+            });
+            setIsSubscribed(true)
+        } catch (error) {
+            console.error("failed to subscribe: ", error)
+        }
 
         // update is state
-        setIsSubscribed(true)
         setLoading(false)
     }
 
@@ -43,7 +47,7 @@ export function NodeState ({ wsAddress, updateStuff }) {
 
             { loading && (
                 <svg className="w-5 h-5 mr-3 -ml-1 text-white animate-spin" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                    <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                    <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
                     <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
                 </svg>
             )}
