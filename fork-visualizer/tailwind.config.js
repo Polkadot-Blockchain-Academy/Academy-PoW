d7ad597e0d0c19b400e0a5a10a31063b3ab79d4a
Kevin Coxe <kevcoxe@gmail.com>
2024-01-05 10:07:23 -0500

    Apply suggestions from code review
    Co-authored-by: Nikos Kontakis <wirednkod@gmail.com>

--- fork-visualizer/tailwind.config.js
+++ fork-visualizer/tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-      './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-      './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-      './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+      './src/pages/**/*.{js,jsx}',
+      './src/components/**/*.{js,jsx}',
+      './src/app/**/*.{js,jsx}',
     ],
     theme: {
       extend: {
