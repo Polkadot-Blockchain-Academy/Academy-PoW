a470e3b705a6ca18848f23c8514aaf5546e7d040
Joshy Orndorff <admin@joshyorndorff.com>
2024-01-12 07:41:38 +0800

    Make faucet free

--- runtime/src/faucet.rs
+++ runtime/src/faucet.rs
@@ -29,7 +29,7 @@ pub mod pallet {
     #[pallet::call]
     impl<T: Config> Pallet<T> {
         /// Claim a few tokens from the faucet
-        #[pallet::weight(1_000_000)]
+        #[pallet::weight((Weight::from(0), Pays::No))]
         pub fn claim(origin: OriginFor<T>) -> DispatchResult {
             let caller = ensure_signed(origin)?;
 
